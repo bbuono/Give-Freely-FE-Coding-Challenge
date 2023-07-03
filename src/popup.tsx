f621a864220c71241bf43d5170e51008c87d2a08
Federico Hoerth <fhoerth@matterway.io>
2023-07-03 08:55:23 -0300

    Check if tailwind css works

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,28 +1,14 @@
-import { useState } from "react"
+import { CountButton } from '~features/CountButton';
+
+import '~base.css';
+import '~style.css';
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="flex items-center justify-center h-16 w-40">
+      <CountButton />
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
