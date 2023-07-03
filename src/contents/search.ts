8ce8b7b5f134c34d2b29209e57aeca4ebce74908
Federico Hoerth <fhoerth@matterway.io>
2023-07-03 09:29:25 -0300

    Remove json file. Create google-sites-config

--- src/contents/search.ts
+++ src/contents/search.ts
@@ -1,11 +1,6 @@
-import type { PlasmoCSConfig } from 'plasmo';
-
 import { onDomContentLoaded } from '~contents-utils/onDomContentLoaded';
-import googleSites from '~google-sites/sites.json';
 
-export const config: PlasmoCSConfig = {
-  matches: googleSites,
-};
+import '~google-sites-config';
 
 async function domContentLoaded(): Promise<void> {
   console.log('DOM Content Loaded - search');
---
+++ src/google-sites-config/index.ts
@@ -0,0 +1,5 @@
+import type { PlasmoCSConfig } from 'plasmo';
+
+export const config: PlasmoCSConfig = {
+  matches: ['https://*.google.com/search*', 'https://*.google.com.ar/search*'],
+};
