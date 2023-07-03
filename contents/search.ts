2547429e69b6fdb9a4dd390ee5588c2da5866db3
Federico Hoerth <fhoerth@matterway.io>
2023-07-03 08:11:42 -0300

    Create google-sites JSON file

---
+++ contents/search.ts
@@ -0,0 +1,21 @@
+import type { PlasmoCSConfig } from 'plasmo';
+
+import googleSites from '~google-sites/sites.json';
+
+export const config: PlasmoCSConfig = {
+  matches: googleSites,
+};
+
+async function domContentLoaded(): Promise<void> {
+  console.log('DOM Content Loaded');
+}
+
+function main(): void {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', domContentLoaded);
+  } else {
+    domContentLoaded();
+  }
+}
+
+void main();
