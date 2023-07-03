bca379681533390ffc95e3e4ed9beabfed0033cd
Federico Hoerth <fhoerth@matterway.io>
2023-07-03 10:56:05 -0300

    Remove console.log from popup.tsx

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,8 +3,6 @@ import { CountButton } from '~features/CountButton';
 import '~base.css';
 import '~style.css';
 
-console.log('From popup', process.env.PLASMO_PUBLIC_PARTICIPANTS_API_URL);
-
 function IndexPopup() {
   return (
     <div className="flex items-center justify-center h-16 w-40">
